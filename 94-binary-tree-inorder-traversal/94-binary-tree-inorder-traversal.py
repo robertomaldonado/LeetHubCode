00dff62a27398809300c0639cae15189e2accf57
Roberto Maldonado <robertomaldonado@users.noreply.github.com>
2021-09-02 23:27:12 -0400

    Time: 55 ms (6.13%), Space: 14.3 MB (13.86%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -8,7 +8,7 @@
 """
 Try recursive approach.
 
-BFS. Using stack, as stack can be used we do the recursive approach.
+DFS. Using stack, as stack can be used we do the recursive approach.
 
       1
     /   \ 
@@ -16,22 +16,31 @@ BFS. Using stack, as stack can be used we do the recursive approach.
  / \   /  \
 5   9 10  11
 
--------------------|
-1                  |
--------------------|
+[5,2,9,1,10,4,11]
 
--------------------|
-2, 4               |
--------------------|
-
--------------------|
-5,9,10,11          |
--------------------|
-
+Inorder: Means left, parent, right
+s1.
+           11| 
+s2.
+           4,1|
 """
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+      if not root: return
+      cur_node = root
+      s1 = []
+      ans = []
+      while cur_node or len(s1) != 0:
+        while cur_node:
+          s1.insert(0,cur_node)
+          cur_node = cur_node.left
+          print(cur_node)
+        cur_node = s1.pop(0)
+        ans.insert(0, cur_node.val)
+        cur_node = cur_node.right
+      return ans[::-1]
       
+      """Recursive approach:
       def _traverse(root, ans):
         if not root: 
           return
@@ -44,4 +53,15 @@ class Solution:
       ans =[]
       _traverse(root, ans)
       return  ans
-        
+      """
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
